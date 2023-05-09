69e397664b57c5aee743f31e6aac03348fcd9f20
Tejas <sawanttejasar@gmail.com>
2023-05-09 15:13:53 +0530

    used Input.GetAxis() method to move the player

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    public float xValue=0;
-    public float yValue=0.01f;
-    [SerializeField] private float zValue=0;
+    // [SerializeField] float xValue=0;
+    // [SerializeField] float yValue=0.01f;
+    // [SerializeField] float zValue=0;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +15,11 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float xValue=Input.GetAxis("Horizontal");
+        float yValue=0; 
+        float zValue=Input.GetAxis("Vertical");
         transform.Translate(xValue, yValue, zValue);
+        // transform.Translate(xValue, yValue, zValue);
+
     }
 }
