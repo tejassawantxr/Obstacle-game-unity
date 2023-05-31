c57d7fb61e619294eb3e929c609dbf25cd4dc4af
Tejas <sawanttejasar@gmail.com>
2023-05-31 15:22:01 +0530

    created a function for moving  functionality of a player

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,11 +4,9 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    // [SerializeField] float xValue=0;
-    // [SerializeField] float yValue=0.01f;
-    // [SerializeField] float zValue=0;
-    // Start is called before the first frame update
+
     [SerializeField] float moveSpeed = 10f;
+
     void Start()
     {
     }
@@ -16,11 +14,14 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       MovePlayer();
+    }
+
+    void MovePlayer(){
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue=0; 
         float zValue=Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        transform.Translate(xValue, yValue, zValue);
-        // transform.Translate(xValue, yValue, zValue);
 
+        transform.Translate(xValue, yValue, zValue);
     }
 }
