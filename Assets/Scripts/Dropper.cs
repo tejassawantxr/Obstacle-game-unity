7f6999dc72f89a4a21924412dce5c44cff114287
Tejas <sawanttejasar@gmail.com>
2023-06-08 19:21:03 +0530

    Made Dropper object invisible and floating in the air and after some a specific time made it visible and fall on the ground

--- Assets/Scripts/Dropper.cs
+++ Assets/Scripts/Dropper.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
+    MeshRenderer renderer;
+    Rigidbody rigidBody;
+    [SerializeField] float timeToWait = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        renderer =  GetComponent<MeshRenderer>();
+        rigidBody = GetComponent<Rigidbody>();
+
+        renderer.enabled = false;
+        rigidBody.useGravity=false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("Time elasped: "+ Time.time);
+        if(Time.time > timeToWait){
+            renderer.enabled = true;
+            rigidBody.useGravity = true;
+        }
     }
 }
